428bb0901388c218e2d81cb32343c31a4987c07c
Matheus Ferreira <matheusfernandofreire@hotmail.com>
2025-11-27 22:39:01 -0300

    feat: adicionar configuração do Redis e módulos de limitação de requisições
    - Criação do arquivo .env.example com variáveis de configuração do Redis.
    - Adição do docker-compose.yml para configuração do serviço Redis.
    - Atualização do package.json e pnpm-lock.yaml para incluir dependências do throttler e Redis.
    - Criação do pnpm-workspace.yaml para gerenciamento de pacotes.
    - Implementação do ThrottlerModule no AppModule com armazenamento em Redis.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,49 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRootAsync({
+      useFactory: (
+        config: ConfigService<
+          {
+            THROTTLER_TTL: number;
+            THROTTLER_LIMIT: number;
+            REDIS_HOST: string;
+            REDIS_PORT: number;
+          },
+          false
+        >,
+      ) => ({
+        throttlers: [
+          {
+            ttl: config.get<number>('THROTTLER_TTL', 60),
+            limit: config.get<number>('THROTTLER_LIMIT', 10),
+          },
+        ],
+        storage: new ThrottlerStorageRedisService({
+          host: config.get<string>('REDIS_HOST', 'localhost'),
+          port: config.get<number>('REDIS_PORT', 6379),
+        }),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
